5941b865e08b8c5d08546fe3e96476208381d592
YASSINE-AA <overflowtn@gmail.com>
2026-04-02 16:13:21 +0100

    Added: Map widget

--- patch_map2.py
+++
@@ -1,7 +0,0 @@
-with open('/home/yassine/AromaUI/src/widgets/aroma_map.c', 'r') as f:
-    text = f.read()
-
-text = text.replace('aroma_event_subscribe(node->node_id, EVENT_TYPE_MOUSE_CLICK, __map_event_handler, NULL, 90);', 'aroma_event_subscribe(node->node_id, EVENT_TYPE_MOUSE_CLICK, __map_event_handler, NULL, 90);\n    aroma_event_subscribe(node->node_id, EVENT_TYPE_MOUSE_DOUBLE_CLICK, __map_event_handler, NULL, 90);')
-
-with open('/home/yassine/AromaUI/src/widgets/aroma_map.c', 'w') as f:
-    f.write(text)
