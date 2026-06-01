ec5247b085054490153a30d6ba80367a9778865f
YASSINE-AA <overflowtn@gmail.com>
2026-06-01 20:38:24 +0100

    working on infotainment example

--- examples/map_example/main.c
+++ examples/map_example/main.c
@@ -10,7 +10,7 @@ int main()
     AromaTheme theme = aroma_theme_create_material_blue();
     aroma_ui_set_theme(&theme);
     AromaWindow *window = aroma_ui_create_window("Map Example", 700, 400);
-
+    AromaFont *text_font = aroma_font_create_from_memory(aroma_ubuntu_ttf, aroma_ubuntu_ttf_len, 14);
     AromaNode *map = aroma_ui_map((AromaNode *)window, 0, 0, 700, 400);
     // aroma_map_set_zoom(map, 12);
     if (map)
@@ -20,6 +20,16 @@ int main()
         aroma_map_add_popup_marker(map, 37.7749f, -122.4194f, 0x0000FF, "San Francisco is a city in California, USA."); // Popup marker at San Francisco
         aroma_map_set_route(map, 48.8566, 2.3522, 48.8049, 2.1204, 0xFF35A8FE);
     }
+    AromaFont *icon_font = aroma_font_create_from_memory(icon_ttf, icon_ttf_len, 16);
+    AromaNode *zoom_in_button = aroma_ui_iconbutton((AromaNode *)window, AROMA_ICON_ADD, 650, 10, 40, ICON_BUTTON_FILLED, NULL, NULL, icon_font);
+    AromaNode *zoom_out_button = aroma_ui_iconbutton((AromaNode *)window, AROMA_ICON_REMOVE, 650, 60, 40, ICON_BUTTON_FILLED, NULL, NULL, icon_font);
+    AromaNode *bottom_card = aroma_ui_card((AromaNode *)window, 10, 340, 440, 40, CARD_TYPE_ELEVATED);
+    AromaNode *card_label = aroma_ui_label(bottom_card, "This is a map of the world with markers and a route. Built in C with AromaUI.", 10, 10, LABEL_STYLE_LABEL_MEDIUM, text_font);
+
+    aroma_node_set_z_index(zoom_in_button, 99999);
+    aroma_node_set_z_index(zoom_out_button, 99999);
+        aroma_node_set_z_index(bottom_card, 99999);
+        aroma_node_set_z_index(card_label, 99999);
 
     while (aroma_ui_is_running())
     {
