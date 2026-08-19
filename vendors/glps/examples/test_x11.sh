3a558e90c928f8f911ebb7aa0b89488db5a8fdfe
Yassine AHMED ALI <overflowtn@gmail.com>
2026-08-19 23:39:14 +0100

    Remove old vendors directories before re-adding as submodules

--- vendors/glps/examples/test_x11.sh
+++
@@ -1,1 +0,0 @@
-cd .. && cmake -S . -B build && cd build && make && sudo make install && cd .. && cd examples && gcc x11_window.c -o test -lGLESv2 -lGLPS -L/usr/local/include  -lm && chmod +x test && ./test
