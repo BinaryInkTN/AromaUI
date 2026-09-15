451e44f53a9d25715562e2442eb794aaf75de597
Yassine AHMED ALI <overflowtn@gmail.com>
2026-09-15 13:02:34 +0100

    3d

--- examples/ide_example/run_ide.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run AromaUI IDE - Electron desktop app
-# Unset ELECTRON_RUN_AS_NODE to ensure Electron initializes properly
-DIR="$(cd "$(dirname "$0")" && pwd)"
-exec env -u ELECTRON_RUN_AS_NODE -u ELECTRON_NO_ATTACH_CONSOLE \
-  "$DIR/node_modules/electron/dist/electron" "$DIR" "$@"
